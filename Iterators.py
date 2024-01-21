3620d3e5b67e9e3af2c68f5154a7e8abd57b2ac0
NikiLis26 <120452720+NikiLis26@users.noreply.github.com>
2024-01-21 11:58:06 +0300

    first commit

---
+++ Iterators.py
@@ -0,0 +1,74 @@
+#itareble что то итерируемое 
+#iterator итератор 
+
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.flat_list = self.flattener(list_of_list)
+        self.cursor = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.cursor += 1
+        if self.cursor == len(self.flat_list):
+            raise StopIteration
+        return self.flat_list[self.cursor]
+
+    def flattener(self, some_list):
+        flattened_list = []
+        for item in some_list:
+            flattened_list.extend(item)
+        return flattened_list
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
+
+import types
+
+def flat_generator(list_of_lists):
+    for sublist in list_of_lists:
+        for item in sublist:
+            yield item
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
